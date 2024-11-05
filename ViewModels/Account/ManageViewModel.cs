5250de4cdd4e4fc4cf7c6246b97c4c7292a13c54
nhatriet <nhatriet2311@gmail.com>
2024-11-05 11:04:11 +0700

    fix change password

--- ViewModels/Account/ManageViewModel.cs
+++ ViewModels/Account/ManageViewModel.cs
@@ -4,24 +4,11 @@ namespace StudyResource.ViewModels.Account
 {
     public class ManageViewModel
     {
-        /*        public ProfileViewModel Manage { get; set; } = new ProfileViewModel();
-                public ChangePasswordViewModel ChangePassword { get; set; } = new ChangePasswordViewModel();*/
-        public string UserName { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
-        public string Email { get; set; }
-
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        [DataType(DataType.PhoneNumber)]
-        [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
-        public string PhoneNumber { get; set; }
+        public ProfileViewModel Manage { get; set; } = new ProfileViewModel();
+        public ChangePasswordViewModel ChangePassword { get; set; } = new ChangePasswordViewModel();
     }
 
-/*    public class ProfileViewModel
+    public class ProfileViewModel
     {
         public string UserName { get; set; }
 
@@ -52,5 +39,5 @@ namespace StudyResource.ViewModels.Account
         [DataType(DataType.Password)]
         [Compare("NewPassword", ErrorMessage = "Mật khẩu xác nhận không khớp.")]
         public string ConfirmPassword { get; set; }
-    }*/
+    }
 }
